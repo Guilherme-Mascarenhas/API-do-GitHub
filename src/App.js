407999ed0f368a191f6bb645ffcf0b6d45414f67
Guilherme-Mascarenhas <guillherme.s.mascarenhas03@gmail.com>
2023-07-06 14:30:34 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Cabecalho from "./Components/Cabecalho.js";
+import InfoPerfil from "./Components/InfoPerfil.js";
 
-function App() {
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
 
-export default App;
+    <Cabecalho />
+    <InfoPerfil />  
+    </>
+  )
+}
+export default App
---
+++ src/Components/Cabecalho.js
@@ -0,0 +1,11 @@
+import './Cabecalho.css'
+
+function Cabecalho (){
+    return (
+        <>
+        <div className="cabecalho">GITHUB API</div>
+        <div className='line'></div>
+        </>
+    )
+}
+export default Cabecalho
